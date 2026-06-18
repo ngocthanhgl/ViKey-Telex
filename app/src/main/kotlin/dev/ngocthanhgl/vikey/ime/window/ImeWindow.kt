1394472bbb6b1bbc5b75dc97f46f3e635b33737f
ngocthanhgl <ngocthanhgl@users.noreply.github.com>
2026-06-18 20:04:46 +0700

    fix: duplicate Modifier import + remove @Composable stringRes from non-composable valueLabel

--- app/src/main/kotlin/dev/ngocthanhgl/vikey/ime/window/ImeWindow.kt
+++ app/src/main/kotlin/dev/ngocthanhgl/vikey/ime/window/ImeWindow.kt
@@ -40,7 +40,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
