ee8fb72f2d4ed286b3bb5cbf7486426a1578dfc2
user <user@dev.local>
2026-06-30 13:49:43 +0700

    fix: add missing remember import in Smartbar.kt

--- app/src/main/kotlin/dev/ngocthanhgl/vikey/ime/smartbar/Smartbar.kt
+++ app/src/main/kotlin/dev/ngocthanhgl/vikey/ime/smartbar/Smartbar.kt
@@ -50,6 +50,7 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
