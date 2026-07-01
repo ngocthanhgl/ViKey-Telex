fbf8fa1e2d25635c1c8900bf0347810f6fde273c
ngocthanhgl <ngocthanhgl@users.noreply.github.com>
2026-07-01 22:08:37 +0700

    fix: add missing takeOrElse import in SnyggBox.kt

--- lib/snygg/src/main/kotlin/org/florisboard/lib/snygg/ui/SnyggBox.kt
+++ lib/snygg/src/main/kotlin/org/florisboard/lib/snygg/ui/SnyggBox.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.takeOrElse
 import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.allowHardware
